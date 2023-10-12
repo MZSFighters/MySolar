3d32a834312e8427bcaa81ff7c96d3004a6c84dc
Yoni Oudmayer <yoniOudmayer@gmail.com>
2023-10-12 10:43:57 +0200

    commit async loading graph

--- lib/graph/graph.dart
+++ lib/graph/graph.dart
@@ -1,170 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:fl_chart/fl_chart.dart';
+import 'package:mysolar/graph/graphDisplay.dart';
 
 class BatteryGraph extends StatelessWidget {
-  final List<double> hourlyKw;
-  final List<List<String>> hourlyAppliances;
+  final double batterySize;
+  final Future<List<Map<String, dynamic>>> hourlyKwFuture;
+  final Future<List<List<String>>> hourlyAppliancesFuture;
 
-  BatteryGraph({required this.hourlyKw, required this.hourlyAppliances});
+  BatteryGraph({
+    required this.batterySize,
+    required this.hourlyKwFuture,
+    required this.hourlyAppliancesFuture,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Line Chart Demo'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            // Text(
-            //   "Predicted battery production minus consumption throughout the day",
-            //   style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            //   textAlign: TextAlign.center,
-            // ),
-            //SizedBox(height: 20), ///////////////////////////////////////////////////
-            Expanded(
-              child: Container(
-                  child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Container(
-                    width: 24 * 60.0, // 24 data points * 60 pixels each
-                    child: LineChart(
-                      LineChartData(
-                        gridData: FlGridData(
-                          show: true,
-                          getDrawingHorizontalLine: (value) {
-                            if (value == 0) {
-                              return FlLine(
-                                color: Colors
-                                    .black, // Color of the horizontal line
-                                strokeWidth: 1, // Width of the horizontal line
-                              );
-                            }
-                            return FlLine(
-                              color: Colors
-                                  .transparent, // No other horizontal lines
-                              strokeWidth: 0,
-                            );
-                          },
-                        ),
-                        titlesData: FlTitlesData(
-                          show: true,
-                          bottomTitles: SideTitles(
-                            showTitles: true,
-                            reservedSize: 22,
-                            interval: 1,
-                            getTextStyles: (context, value) => const TextStyle(
-                              color: Color(0xff68737d),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                            getTitles: (value) {
-                              return '${value.toInt()}:00';
-                            },
-                            margin: 8,
-                          ),
-                          leftTitles: SideTitles(
-                            showTitles: true,
-                            reservedSize: 40,
-                            getTextStyles: (context, value) => const TextStyle(
-                              color: Color(0xff67727d),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                            getTitles: (value) {
-                              switch (value.toInt()) {
-                                case 10:
-                                  return '10kw';
-                                case 5:
-                                  return '5kw';
-                                case 0:
-                                  return '0kw';
-                                case -5:
-                                  return '-5kw';
-                                case -10:
-                                  return '-10kw';
-                                default:
-                                  return '';
-                              }
-                            },
-                            margin: 12,
-                          ),
-                        ),
-                        borderData: FlBorderData(
-                          show: true,
-                          border: Border.all(
-                              color: const Color(0xff37434d), width: 1),
-                        ),
-                        minX: 0,
-                        maxX: 24,
-                        minY: -14,
-                        maxY: 14,
-                        lineTouchData: LineTouchData(
-                          touchTooltipData: LineTouchTooltipData(
-                            tooltipBgColor: Colors.blueAccent,
-                            getTooltipItems:
-                                (List<LineBarSpot> touchedBarSpots) {
-                              return touchedBarSpots.map((barSpot) {
-                                final flSpot = barSpot;
-                                final appliancesForHour =
-                                    hourlyAppliances[flSpot.x.toInt()]
-                                        .join(', ');
-                                return LineTooltipItem(
-                                  /* 'Hour ${flSpot.x.toInt()}: */ '${flSpot.y}kw\nAppliances used: $appliancesForHour',
-                                  const TextStyle(color: Colors.white),
-                                );
-                              }).toList();
-                            },
-                          ),
-                          touchCallback: (LineTouchResponse touchResponse) {},
-                          handleBuiltInTouches: true,
-                        ),
-                        lineBarsData: [
-                          LineChartBarData(
-                            spots: generateData(),
-                            isCurved: true,
-                            colors: [Colors.black.withOpacity(0.5)],
-                            barWidth: 4,
-                            isStrokeCapRound: true,
-                            aboveBarData: BarAreaData(
-                              show: true,
-                              colors: generateData()
-                                  .map((spot) => spot.y < 0
-                                      ? Colors.orange.withOpacity(0.8)
-                                      : Colors.transparent)
-                                  .toList(),
-                              cutOffY: 0.0,
-                              applyCutOffY: true,
-                            ),
-                            belowBarData: BarAreaData(
-                              show: true,
-                              colors: generateData()
-                                  .map((spot) => spot.y >= 0
-                                      ? Colors.green.withOpacity(0.8)
-                                      : Colors.transparent)
-                                  .toList(),
-                              cutOffY: 0.0,
-                              applyCutOffY: true,
-                            ),
-                            dotData: FlDotData(show: true),
-                          ),
-                        ],
-                      ),
-                    )),
-              )),
-            ),
-          ],
-        ),
-      ),
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: hourlyKwFuture,
+      builder: (context, snapshotKw) {
+        if (snapshotKw.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshotKw.hasError) {
+          return Scaffold(
+            body: Center(child: Text('Error: ${snapshotKw.error}')),
+          );
+        } else {
+          return FutureBuilder<List<List<String>>>(
+            future: hourlyAppliancesFuture,
+            builder: (context, snapshotAppliances) {
+              if (snapshotAppliances.connectionState == ConnectionState.waiting) {
+                return Scaffold(
+                  body: Center(child: CircularProgressIndicator()),
+                );
+              } else if (snapshotAppliances.hasError) {
+                return Scaffold(
+                  body: Center(child: Text('Error: ${snapshotAppliances.error}')),
+                );
+              } else {
+                List<Map<String, dynamic>> hourlyKw = snapshotKw.data!;
+                List<List<String>> hourlyAppliances = snapshotAppliances.data!;
+                return BatteryGraphDisplay(batterySize: batterySize, hourlyKw: hourlyKw, hourlyAppliances: hourlyAppliances); 
+              }
+            },
+          );
+        }
+      },
     );
   }
-
-  List<FlSpot> generateData() {
-    List<FlSpot> data = [];
-    for (int i = 0; i <= 24; i++) {
-      data.add(FlSpot(i.toDouble(), hourlyKw[i]));
-    }
-    return data;
-  }
 }
+
+
---
+++ lib/graph/graphDisplay.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class BatteryGraphDisplay extends StatelessWidget {
+  final double batterySize;
+  final List<Map<String, dynamic>> hourlyKw;
+  final List<List<String>> hourlyAppliances;
+
+  BatteryGraphDisplay({
+    required this.batterySize,
+    required this.hourlyKw,
+    required this.hourlyAppliances,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Usage Graph'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+         child: Column(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: <Widget>[
+      // Title
+      Text(
+        "Predicted Battery And Grid Usage",
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
+      SizedBox(height: 10), // Spacing
+      // Key for Battery Usage
+      Row(
+        children: [
+          Container(
+            width: 20,
+            height: 20,
+            color: Colors.green,
+          ),
+          SizedBox(width: 10), // Spacing between the square and the text
+          Text("- Battery Usage"),
+        ],
+      ),
+      SizedBox(height: 10), // Spacing
+      // Key for Grid Usage
+      Row(
+        children: [
+          Container(
+            width: 20,
+            height: 20,
+            color: Colors.orange,
+          ),
+          SizedBox(width: 10), // Spacing between the square and the text
+          Text("- Grid Usage"),
+        ],
+      ),
+      SizedBox(height: 20),
+      Expanded( 
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Container(
+                    width: 12 * 60.0, // 12 data points * 60 pixels each
+                    child: LineChart(
+                      LineChartData(
+                        gridData: FlGridData(
+                          show: true,
+                          getDrawingHorizontalLine: (value) {
+                            if (value == 0) {
+                              return FlLine(
+                                color: Colors.black,
+                                strokeWidth: 1,
+                              );
+                            }
+                            return FlLine(
+                              color: Colors.transparent,
+                              strokeWidth: 0,
+                            );
+                          },
+                        ),
+                        titlesData: FlTitlesData(
+                          show: true,
+                          bottomTitles: SideTitles(
+                            showTitles: true,
+                            reservedSize: 22,
+                            interval: 1,
+                            getTextStyles: (context, value) => const TextStyle(
+                              color: Color(0xff68737d),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                            getTitles: (value) {
+                              return hourlyKw[value.toInt()]['hour'];
+                            },
+                            margin: 8,
+                          ),
+                          leftTitles: SideTitles(
+                            showTitles: true,
+                            reservedSize: 40,
+                            getTextStyles: (context, value) => const TextStyle(
+                              color: Color(0xff67727d),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                            getTitles: (value) {
+                              switch (value.toInt()) {
+                                case 10:
+                                  return '10kw';
+                                case 5:
+                                  return '5kw';
+                                case 0:
+                                  return '0kw';
+                                case -5:
+                                  return '-5kw';
+                                case -10:
+                                  return '-10kw';
+                                default:
+                                  return '';
+                              }
+                            },
+                            margin: 12,
+                          ),
+                        ),
+                        borderData: FlBorderData(
+                          show: true,
+                          border: Border.all(
+                              color: const Color(0xff37434d), width: 1),
+                        ),
+                        minX: 0,
+                        maxX: 11,
+                        minY: -14,
+                        maxY: 14,
+                        lineTouchData: LineTouchData(
+                          touchTooltipData: LineTouchTooltipData(
+                            tooltipBgColor: Colors.blueAccent,
+                            getTooltipItems:
+                                (List<LineBarSpot> touchedBarSpots) {
+                              return touchedBarSpots.map((barSpot) {
+                                final flSpot = barSpot;
+                                final appliancesForHour =
+                                    hourlyAppliances[flSpot.x.toInt()]
+                                        .join(', ');
+                                return LineTooltipItem(
+                                  '${flSpot.y}kw\nAppliances used: $appliancesForHour',
+                                  const TextStyle(color: Colors.white),
+                                );
+                              }).toList();
+                            },
+                          ),
+                          touchCallback: (LineTouchResponse touchResponse) {},
+                          handleBuiltInTouches: true,
+                        ),
+                        lineBarsData: [
+                          LineChartBarData(
+                            spots: generateData(),
+                            isCurved: true,
+                            colors: [Colors.black.withOpacity(0.5)],
+                            barWidth: 4,
+                            isStrokeCapRound: true,
+                            aboveBarData: BarAreaData(
+                              show: true,
+                              colors: generateData()
+                                  .map((spot) => spot.y < 0
+                                      ? Colors.orange.withOpacity(0.8)
+                                      : Colors.transparent)
+                                  .toList(),
+                              cutOffY: 0.0,
+                              applyCutOffY: true,
+                            ),
+                            belowBarData: BarAreaData(
+                              show: true,
+                              colors: generateData()
+                                  .map((spot) => spot.y >= 0
+                                      ? Colors.green.withOpacity(0.8)
+                                      : Colors.transparent)
+                                  .toList(),
+                              cutOffY: 0.0,
+                              applyCutOffY: true,
+                            ),
+                            dotData: FlDotData(show: true),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+    ),
+  ),
+  );
+}
+
+  List<FlSpot> generateData() {
+    List<FlSpot> data = [];
+    for (int i = 0; i < 12; i++) {
+      data.add(FlSpot(i.toDouble(), hourlyKw[i]['outputKw']));
+    }
+    return data;
+  }
+}
