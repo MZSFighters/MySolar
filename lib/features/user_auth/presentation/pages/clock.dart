77eb322ec1140bf0a51021f7726e3ba0180e2637
morbidlyObese <muhammad.a.omar.97@gmail.com>
2023-10-20 08:20:11 +0200

    Update clock.dart

---
+++ lib/features/user_auth/presentation/pages/clock.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:one_clock/one_clock.dart';
+
+class MyAnalogClock extends StatelessWidget {
+  const MyAnalogClock({super.key});
+
+  @override
+  Widget build(BuildContext context) => Container(
+    padding: EdgeInsets.only(
+      top: 5 + MediaQuery.of(context).padding.top,
+      bottom: 24,
+    ),
+    child: Expanded(
+      child: AnalogClock(
+                decoration: BoxDecoration(
+                border: Border.all(width: 2.0 ,color: Colors.black),
+                color: Colors.transparent,
+                shape: BoxShape.circle),
+              width: 150.0,
+              isLive: true,
+              hourHandColor: Colors.black,
+              minuteHandColor: Colors.black,
+              showSecondHand: true,
+              secondHandColor: Colors.red,
+              numberColor: Colors.black87,
+              showNumbers: true,
+              showAllNumbers: false,
+              textScaleFactor: 1.4,
+              showTicks: true,
+              showDigitalClock: true,
+      ),
+    ),
+  );
+}
