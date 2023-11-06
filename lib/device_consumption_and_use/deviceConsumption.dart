2760e293ec4b96d418f8d3884fb81539636abc3e
Mohammad Zaid Moonsamy <127732471+MZSFighters@users.noreply.github.com>
2023-11-06 20:20:39 +0200

    added consumption and device for each minute

--- lib/device_consumption_and_use/deviceConsumption.dart
+++ lib/device_consumption_and_use/deviceConsumption.dart
@@ -92,7 +92,93 @@ for (int i = 0; i < 12; i++) {
   return devicesOn;
 }
 
+  Future<List<double>> calculateMinuteConsumption() async { 
+    QuerySnapshot appliancesSnapshot = await firestore
+        .collection('appliances')
+        .where('userId', isEqualTo: userId)
+        .get();
+    List<Map<String, dynamic>> appliances = appliancesSnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
 
+    List<double> minuteConsumption = List.filled(720, 0.0);
+    DateTime now = DateTime.now();
+    int currentHour = now.hour;
+    int currentMinute = now.minute;
+    
+ for (int i = 0; i < 720; i++) {
+    int hourToCheck = (currentHour+1 + i ~/ 60) % 24;
+    int minuteToCheck = i % 60;
+
+    for (var appliance in appliances) {
+      Map<String, dynamic> time = appliance['time'];
+      int startTime = time['startTime'];
+      int endTime = time['endTime'];
+      bool sameDay = time['sameDay'];
+
+      int startHour = startTime ~/ 60;
+      int endHour = endTime ~/ 60;
+      int startMinute = startTime % 60;
+      int endMinute = endTime % 60;
+
+      if (hourToCheck == startHour && hourToCheck == endHour) {
+        if (minuteToCheck >= startMinute && minuteToCheck <= endMinute) {
+          minuteConsumption[i] += appliance['kw'] / 60.0;
+        }
+      } else if (hourToCheck == startHour) {
+        if (minuteToCheck >= startMinute) {
+          minuteConsumption[i] += appliance['kw'] / 60.0;
+        }
+      } else if (hourToCheck == endHour) {
+        if (minuteToCheck <= endMinute) {
+          minuteConsumption[i] += appliance['kw'] / 60.0;
+        }
+      } else if (sameDay && hourToCheck > startHour && hourToCheck < endHour) {
+        minuteConsumption[i] += appliance['kw'] / 60.0;
+      } else if (!sameDay && (hourToCheck > startHour || hourToCheck < endHour)) {
+        minuteConsumption[i] += appliance['kw'] / 60.0;
+      }
+    }
+  }
+    return minuteConsumption;
+}
+
+
+Future<List<List<String>>> devicesOnEachMinute() async {
+   QuerySnapshot appliancesSnapshot = await firestore
+        .collection('appliances')
+        .where('userId', isEqualTo: userId)
+        .get();
+  List<Map<String, dynamic>> appliances = appliancesSnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+
+ // print("Fetched appliances: $appliances"); // Debug print
+
+  List<List<String>> devicesOn = List.generate(720, (index) => <String>[]);
+  DateTime now = DateTime.now();
+  int currentHour = now.hour;
+
+for (int i = 0; i < 720; i++) {
+  int hourToCheck = (currentHour+1 + i ~/ 60) % 24;
+  for (var appliance in appliances) {
+    Map<String, dynamic> time = appliance['time'];
+    int startTime = time['startTime'] ~/ 60;
+    int endTime = time['endTime'] ~/ 60;
+    bool sameDay = time['sameDay'];
+  
+
+    if (sameDay) {
+      if (hourToCheck == startTime || (hourToCheck >= startTime && hourToCheck < endTime)) {
+        devicesOn[i].add(appliance['name']);
+      }
+    } else {
+      if (hourToCheck >= startTime || hourToCheck < endTime) {
+        devicesOn[i].add(appliance['name']);
+      }
+    }
+  }
+}
+ // print("Devices on each hour: $devicesOn"); // debug print
+
+  return devicesOn;
+}
 
 
 }
