22fd6fad689454be56a02ea2ca9ee2dfb6bec23a
Yoni Oudmayer <yoniOudmayer@gmail.com>
2023-11-07 17:41:55 +0200

    change graph to line

--- lib/graph/graph_initiator.dart
+++ lib/graph/graph_initiator.dart
@@ -5,29 +5,44 @@ import 'package:flutter/material.dart';
 import 'package:mysolar/battery_output_calculation/finalOutputCalculation.dart';
 import 'package:mysolar/graph/prediction_graph.dart';
 import 'package:mysolar/load_shedding/fetch_today_schedule.dart';
+import 'package:mysolar/device_consumption_and_use/deviceConsumption.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mysolar/models/device.dart';
+
+final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+final FirebaseAuth auth = FirebaseAuth.instance;
+
+final User? user = auth.currentUser;
+
+final String userId = user!.uid;
+
 
 
 class GraphInitiator extends StatelessWidget {
 
   //final energy usage :
   static Future<List<Map<String, dynamic>>> finalOutputData() async {  
-    List<double> consumptionData = List.generate(720, (index) => Random().nextDouble());
+    //List<double> consumptionData = List.generate(720, (index) => Random().nextDouble());
+   
 
     FinalOutputCalculation finalCalculation = FinalOutputCalculation(
-      batterySize: 8, 
+      batterySize: 5.00, 
       lowestBatteryPercentage: 10, 
-      maxPower: 15.0, 
-      consumptionData: consumptionData, 
+      maxPower: 10.0, 
+      userID: userId,
+      firestore: firestore,
     );
 
     List<Map<String, dynamic>> outputData = await finalCalculation.calculateFinalOutput();
     return outputData;
   }
 
-  final double maxOutput = 15.00;
-  final double possibleStorage = 8.00 * (1 - 10/100);
+  final double maxOutput = 10.00;
+  final double possibleStorage = 5.00 * (1 - 10/100);
   final Future<List<Map<String, dynamic>>> outputData = GraphInitiator.finalOutputData();
-  final Future<List<List<String>>> hourlyAppliancesFuture = Future.value([]);
+  final Future<List<List<String>>> minutelyAppliancesFuture = DeviceConsumption(userId: userId, firestore: firestore).devicesOnEachMinute();
   final Future<List<List<int>>> loadSheddingScheduleFuture = FetchTodaysLoadSheddingSchedule().getCurrentDayLoadShedding();
 
 
@@ -47,7 +62,7 @@ class GraphInitiator extends StatelessWidget {
           );
         } else {
               return FutureBuilder<List<List<String>>>(
-                future: hourlyAppliancesFuture,
+                future: minutelyAppliancesFuture,
                 builder: (context, snapshotAppliances) {
                   if (snapshotAppliances.connectionState == ConnectionState.waiting) {
                     return Scaffold(
@@ -71,13 +86,13 @@ class GraphInitiator extends StatelessWidget {
                                 );
                               }else { 
                                   List<Map<String, dynamic>> hourlyKw = snapshotKw.data!;
-                                  List<List<String>> hourlyAppliances = snapshotAppliances.data!;
+                                  List<List<String>> minutelyAppliances = snapshotAppliances.data!;
                                   List<List<int>>? loadShedding = snapshotLoadShedding.data!;
                                   return PredictionGraph(
                                     maxOutput: maxOutput,
                                     possibleStorage: possibleStorage,
                                     hourlyKw: hourlyKw,
-                                    hourlyAppliances: hourlyAppliances,
+                                    minutelyAppliances: minutelyAppliances,
                                     loadShedding:loadShedding,
                                   ); 
                                 }
